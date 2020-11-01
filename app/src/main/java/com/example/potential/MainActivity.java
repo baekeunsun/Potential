cbaea01c26bfd68935604363e555ffee9a5be97f
wodud2970 <64058784+wodud2970@users.noreply.github.com>
2020-11-02 03:21:44 +0900

    Main

--- app/src/main/java/com/example/potential/MainActivity.java
+++ app/src/main/java/com/example/potential/MainActivity.java
@@ -5,15 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    TextView jin_ju, chang_won;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        jin_ju = findViewById(R.id.jin_ju);
+        chang_won = findViewById(R.id.chang_won);
+
+//Changwon Jinju intent
+        jin_ju.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+        chang_won.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
 
         if(FirebaseAuth.getInstance().getCurrentUser()==null){
             startSignUpActivity();
